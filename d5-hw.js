cc31d479af49f03d646ff4ae06070a3f6e02a96f
hsattar <39243545+hsattar@users.noreply.github.com>
2021-09-24 14:14:58 +0100

    Solved Q1

--- d5-hw.js
+++ d5-hw.js
@@ -12,67 +12,71 @@ ASSIGNMENT RULES
 Write a function called "area" which receives 2 parameters (l1, l2) and calculates the area of the associated rectangle.
 */
 
-console.log('test git')
+console.log('---------- Answer Q1 --------\n')
 
-/* WRITE YOUR ANSWER HERE */
+const area = (l1, l2) => {
+    return l1 * l2
+}
+
+console.log(area(3, 5))
 
 /* EXERCISE 2
 Write a function called "crazySum" which receives two integers as parameters.
 It should return the sum of those two values, but if the two values are the same then it should return their sum multiplied by 3.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log('\n\n---------- Answer Q2 --------\n')
 
 /* EXERCISE 3
 Write a function called "crazyDiff" that computes the absolute difference between a given number and 19.
 It should return triple their absolute difference if the given number is greater than 19.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log('\n\n---------- Answer Q3 --------\n')
 
 /* EXERCISE 4
 Write a function called "boundary" which accept an integer parameter n and returns true if n is within 20 and 100 (included) or if n it's equal to 400.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log('\n\n---------- Answer Q4 --------\n')
 
 /* EXERCISE 5
 Write a function called "strivify" which accepts a string as a parameter.
 It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log('\n\n---------- Answer Q5 --------\n')
 
 /* EXERCISE 6
 Write a function called "check3and7" which accepts a positive number as a parameter and checks if it is a multiple of 3 or a multiple of 7.
 HINT: Modulus Operator
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log('\n\n---------- Answer Q6 --------\n')
 
 /* EXERCISE 7
 Write a function called "reverseString" which programmatically reverses a given string (es.: Strive => evirtS).
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log('\n\n---------- Answer Q7 --------\n')
 
 /* EXERCISE 8
 Write a function called "upperFirst" which capitalizes the first letter of each word of a given string passed as a parameter.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log('\n\n---------- Answer Q8 --------\n')
 
 /* EXERCISE 9
 Write a function called "cutString" which creates a new string without the first and last character of a given string passed as a parameter.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log('\n\n---------- Answer Q9 --------\n')
 
 /* EXERCISE 10
 Write a function called "giveMeRandom" which accepts a number n and returns an array containing n random numbers between 0 and 10.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log('\n\n---------- Answer Q10 --------\n')
 
 /* WHEN YOU ARE FINISHED
 Commit and push the code to your personal GitHub repository; then post the link of your commit on the Homework section of today's Eduflow.
